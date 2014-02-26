e526e11bce35f2c500be7d3e9cf8b27a6eba1553
Sergey Travin <sergey.a.travin@gmail.com>
2014-02-26 16:23:51 +0600

    Add some tests

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -7,9 +7,18 @@ import org.junit.Test;
 import uk.co.placona.helloWorld.HelloWorld;
 
 public class HelloWorldTest {
-	@Test
-	public void testHellowWorld(){
-		HelloWorld hello = new HelloWorld();
-		assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
-	}
+    @Test
+    public void testHellowWorld(){
+        HelloWorld hello = new HelloWorld();
+        assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
+    }
+
+    @Test
+    public void testAssertNull() {
+        assertNull("should be null", null);
+        String str;
+        assertNull("should be null", str);
+        str = "string";
+        assertNull("should be null", str);
+    }
 }
